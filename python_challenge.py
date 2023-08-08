a79d6ff4c5c42fce865c65f63c52997b2556dec8
alexmartinez8295 <alexismh8295@gmail.com>
2023-08-08 14:55:54 -0600

    inicio de ejerciciós de python_challenge

---
+++ python_challenge.py
@@ -0,0 +1,38 @@
+# Nivel 0
+#print(pow(2,38))
+#Resuelto
+
+#Nivel 1
+mapaAbc = {
+    "A":"C",
+    "B":"D",
+    "C":"E",
+    "D":"F",
+    "E":"G",
+    "F":"H",
+    "G":"I",
+    "H":"J",
+    "I":"K",
+    "J":"L",
+    "K":"M",
+    "L":"N",
+    "M":"O",
+    "N":"P",
+    "O":"Q",
+    "P":"R",
+    "Q":"S",
+    "R":"T",
+    "S":"U",
+    "T":"V",
+    "U":"W",
+    "V":"X",
+    "W":"Y",
+    "X":"Z",
+    "Y":"A",
+    "Z":"B"
+}
+
+cadena = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+for element in mapaAbc:
+    print(mapaAbc[element])
